c55492342e109085450c5c168fbc3a2bf737a5f2
VandanGupte101727 <vandan.gupte27@gmail.com>
2024-09-30 09:09:45 +0530

    Add files via upload

---
+++ experiment12a.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+{
+    int rn;
+    char n[50];
+    float avg = 0.0;
+    public:
+    student()
+{
+    cout<<"Enter the name: ";
+    cin>>n;
+    cout<<"Enter the roll no.: ";
+    cin>>rn;
+    cout<<"Enter the average: ";
+    cin>>avg;
+}
+void display()
+{
+    cout<<endl;
+    cout<<"Name: "<<n<<endl;
+    cout<<"Roll No: "<<rn<<endl;
+    cout<<"Average: "<<avg<<"%"<<endl;
+}
+};
+int main()
+{
+    student s1;
+    s1.display();
+}
